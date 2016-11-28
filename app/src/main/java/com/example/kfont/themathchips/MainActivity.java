8d16241248ccdd13e50a4294c689f49919455d3f
Kristofer Fontanez <kfontanez23@yahoo.com>
2016-11-27 20:25:04 -0500

    New Math Chips Android

---
+++ app/src/main/java/com/example/kfont/themathchips/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.kfont.themathchips;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent continueIntent = new Intent(MainActivity.this, MainMenu.class);
+                MainActivity.this.startActivity(continueIntent);
+            }
+        });
+    }
+}
